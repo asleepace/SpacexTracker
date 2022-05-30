f8a3c545a383a82ee467b3b34049e1e4d3960cea
Colin Teahan <colin_teahan@yahoo.com>
2022-05-30 14:06:47 -0700

    added the about me section

--- src/controllers/AboutMeController.tsx
+++ src/controllers/AboutMeController.tsx
@@ -1,18 +1,44 @@
-import React from 'react'
-import {Modal, StyleSheet, View} from 'react-native'
+import React, {useState} from 'react'
+import {Image, Modal, StyleSheet, View} from 'react-native'
 import {LabeledText} from '../components'
 
 interface AboutMeControllerProps {
-  isVisible: boolean
+  shouldShow: boolean
 }
 
-export const AboutMeController = ({isVisible}: AboutMeControllerProps) => {
+interface AboutMeHandle {
+  show(): void
+  hide(): void
+}
+
+export const AboutMeController = (
+  {shouldShow}: AboutMeControllerProps,
+  ref: React.RefObject<AboutMeHandle>,
+) => {
+  const [visible, setVisible] = useState(shouldShow)
+
+  // useImperativeHandle(ref, () => ({
+  //   hide: () => setVisible(false),
+  //   show: () => setVisible(true),
+  // }))
+
   return (
-    <Modal presentationStyle={'formSheet'} visible={isVisible}>
+    <Modal presentationStyle={'formSheet'} visible={visible}>
       <View style={styles.container}>
+        <Image
+          source={require('../../assets/colin.jpeg')}
+          resizeMode={'cover'}
+          style={styles.colin}
+        />
         <LabeledText label={'name'} text={'Colin Teahan'} />
         <LabeledText label={'phone number'} text={'+1 (415) 302-7992'} />
         <LabeledText label={'email address'} text={'colin_teahan@yahoo.com'} />
+        <LabeledText label={'location'} text={'San Francisco, CA'} />
+        <LabeledText label={'github'} text={'https://github.com/asleepace'} />
+        <LabeledText
+          label={'linkedin'}
+          text={'https://www.linkedin.com/in/colin-teahan/'}
+        />
       </View>
     </Modal>
   )
@@ -21,7 +47,14 @@ export const AboutMeController = ({isVisible}: AboutMeControllerProps) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'black',
+    justifyContent: 'space-between',
+    paddingVertical: 60,
     padding: 16,
     flex: 1,
+  },
+  colin: {
+    borderRadius: 50,
+    height: 100,
+    width: 100,
   },
 })
